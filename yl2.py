2b5ee76be9be12d98aac23806c8d848b3f647b47
mardomai <mardo.mai@ametikool.ee>
2022-10-11 13:59:50 +0300

    commit

---
+++ yl2.py
@@ -0,0 +1,13 @@
+# Import math Library
+import math
+
+radius = int(input('Raadius: '))
+
+
+s = math.pi * radius * radius
+
+print('Pindala on', s)
+
+
+p = 2 * math.pi * radius
+print('Ümbermõõt on', p)
