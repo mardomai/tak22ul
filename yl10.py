c024545f1b77f069976e7611f8bc3ce397d69318
mardomai <mardo.mai@ametikool.ee>
2022-10-31 13:41:07 +0200

    commit

--- yl10.py
+++ yl10.py
@@ -5,13 +5,13 @@ print("Tere",name,)
 elukoht = input("Kus sa elad?: ")
 
 if elukoht == "Saaremaa":
-    print("Ah siis sa võid vot minna!")
+    print("Ah norm, mis chillid!")
 
 age = int(input("Kui vana sa oled?: "))
 
 if age < 18:
     print("Liiga noor, et autot juhtida.")
 elif age == 18:
-    print("Palju õnne täisealiseks saamisel!")
+    print("Sa võid legaalselt Karli käppida nüüd!")
 elif age > 18:
     print("Võid autot juhtida.")
---
+++ yl14.py
@@ -0,0 +1,3 @@
+fail = input("Mis on su failinimi?:")
+
+print("Su faililaind on:", fail.split("."))
